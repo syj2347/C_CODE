e55c25eddeaff7245dd4e1060af08cec169394f1
syj22347 <18701700690@163.com>
2021-06-30 23:29:54 +0800

    测试

---
+++ Project1-6.27/Project1-6.27/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	char ch = 'A';
+	float f = 5.0;
+	double d = 3.14;
+	printf("hehe\n");
+	printf("%c\n", ch);
+	printf("%f\n", f);
+	printf("%lf\n",d);
+	return 0;
+}
+
+
+
+
+
